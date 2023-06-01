5838aed7c4632260c13c00b1fa81939e15629622
javier <javibupq@gmail.com>
2023-06-02 00:24:11 +0200

    Hecha pantalla que muestra las faltas de una asignatura (falta que al pulsarle a quitar se ejecute el endpoint)

---
+++ app/src/main/java/com/example/asistelo/adapter/AbsencesOfSubjectAdapter.kt
@@ -0,0 +1,74 @@
+package com.example.asistelo.adapter
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.asistelo.R
+import com.example.asistelo.controllers.dto.AbsenceDto
+import java.text.SimpleDateFormat
+import java.util.*
+
+class AbsencesOfSubjectAdapter(
+    val absences: List<AbsenceDto>,
+    val context: Context,
+) :
+    RecyclerView.Adapter<AbsencesOfSubjectAdapter.ItemViewHolder>() {
+
+    private val layoutInflater = LayoutInflater.from(context)
+
+    public val checkedMap: MutableMap<Int, Boolean> =
+        mutableMapOf() // Mapa para almacenar el estado de marcado/desmarcado
+
+    class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        @SuppressLint("SetTextI18n")
+        fun load(absence: AbsenceDto) {
+            val showStudentNameInAbsencesOfSubject =
+                itemView.findViewById<TextView>(R.id.showStudentNameInAbsencesOfSubject)
+            showStudentNameInAbsencesOfSubject.text = absence.student!!.name
+            val showStudentSurnameInAbsencesOfSubject =
+                itemView.findViewById<TextView>(R.id.showStudentSurnameInAbsencesOfSubject)
+            if (absence.student.secondSurname == null) {
+                showStudentSurnameInAbsencesOfSubject.text =
+                    absence.student.firstSurname
+            } else {
+                showStudentSurnameInAbsencesOfSubject.text =
+                    absence.student.firstSurname + " " + absence.student.secondSurname
+            }
+
+            val showAbsenceDateInAbsencesOfSubject =
+                itemView.findViewById<TextView>(R.id.showAbsenceDateInAbsencesOfSubject)
+            val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+            val formattedDate = dateFormat.format(absence.date!!)
+            showAbsenceDateInAbsencesOfSubject.text = formattedDate
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val absenceOfSubjectLayout =
+            layoutInflater.inflate(R.layout.show_absence_of_subject, null)
+        return ItemViewHolder(absenceOfSubjectLayout)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val absence = absences[position]
+        holder.load(absence)
+
+        val deleteAbsenceCheckBox =
+            holder.itemView.findViewById<CheckBox>(R.id.deleteAbsenceCheckBox)
+        deleteAbsenceCheckBox.isChecked = checkedMap.getOrDefault(absence.id, false)
+
+        deleteAbsenceCheckBox.setOnCheckedChangeListener { _, isChecked ->
+            checkedMap[absence.id!!] = isChecked
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return absences.size
+    }
+}
--- app/src/main/java/com/example/asistelo/screens/ViewAbsencesOfSubjectActivity.kt
+++ app/src/main/java/com/example/asistelo/screens/ViewAbsencesOfSubjectActivity.kt
@@ -2,8 +2,14 @@ package com.example.asistelo.screens
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.asistelo.R
+import com.example.asistelo.adapter.AbsencesOfSubjectAdapter
+import com.example.asistelo.adapter.StudentListForAbsencesAdapter
 import com.example.asistelo.controllers.dto.AbsenceDto
+import com.example.asistelo.decorator.SimpleItemDecoration
 
 class ViewAbsencesOfSubjectActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +17,22 @@ class ViewAbsencesOfSubjectActivity : AppCompatActivity() {
         setContentView(R.layout.activity_view_absences_of_subject)
 
         val absences = intent.getSerializableExtra("absences") as List<AbsenceDto>
+
+        val showSubjectNameInAbsences = findViewById<TextView>(R.id.showSubjectNameInAbsences)
+        showSubjectNameInAbsences.text = absences.get(0).subject!!.name
+
+        val absencesOfSubjectRecyclerView =
+            findViewById<RecyclerView>(R.id.absencesOfSubjectRecyclerView)
+
+        val absencesOfSubjectAdapter =
+            AbsencesOfSubjectAdapter(absences, applicationContext)
+
+        absencesOfSubjectRecyclerView.layoutManager =
+            GridLayoutManager(this, 1, RecyclerView.VERTICAL, false)
+
+        absencesOfSubjectRecyclerView.addItemDecoration(SimpleItemDecoration(applicationContext, 5))
+
+        absencesOfSubjectRecyclerView.adapter = absencesOfSubjectAdapter
 
         //TODO: hacer adapter y recycler view que enseñe las faltas de esa asignatura con un checkbox de borrar
         //TODO: si está marcado el checkbox de borrar, recorrer el endpoint de delete y borrarlo
